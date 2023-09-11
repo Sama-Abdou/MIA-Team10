2d95b50b84eb140b279bdcdcb94e81d8d258fc9e
Mwa195 <134608406+Mwa195@users.noreply.github.com>
2023-09-11 12:54:28 +0300

    Update Cytron.ino

--- Cytron.ino
+++ Cytron.ino
@@ -1,22 +1,22 @@
-#define pwm1 3
-#define dir1 4
+#define pwm1 3 // PWM1 pin connected to digital pin 3
+#define dir1 4  // DIR1 pin connected to digital pin 4
 
-float alpha = 0.1;
-float lastAvg = 0.0;
+float alpha = 0.1; // Alpha constant which is used in the filter
+float lastAvg = 0.0; // A variable to store the last average taken
 
 void setup() {
-  pinMode(pwm1, OUTPUT);
-  pinMode(dir1, OUTPUT);
+  pinMode(pwm1, OUTPUT); // Setting the mode of the PWM1 pin to OUTPUT
+  pinMode(dir1, OUTPUT);  // Setting the mode of the DIR1 pin to OUTPUT
   Serial.begin(9600);
 }
 
 void loop() {
-  byte currentAvg = 0;
-  byte currentValue = 250;
-  currentAvg = (currentValue*alpha) + ((lastAvg)*(1-alpha));
-  lastAvg = currentAvg;
-  digitalWrite(dir1, HIGH);
-  analogWrite(pwm1, currentAvg);
+  byte currentAvg = 0; // A variable to store the current average between 0-255 to be assigned to the PWM1
+  byte currentValue = 250; // The value we want to reach by the end
+  currentAvg = (currentValue*alpha) + ((lastAvg)*(1-alpha)); // The exponential smoothing equation
+  lastAvg = currentAvg; // Setting the last average which will be used in the next loop
+  digitalWrite(dir1, HIGH); // Setting the direction of the motor
+  analogWrite(pwm1, currentAvg); // Setting the speed of the motor
   Serial.println(currentAvg);
   delay(100);
 }
