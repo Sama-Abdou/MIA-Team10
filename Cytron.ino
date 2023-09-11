bbd217241b8fd42ed9c5010554f220fba1ec02c8
Mwa195 <134608406+Mwa195@users.noreply.github.com>
2023-09-11 11:40:50 +0300

    Add files via upload

---
+++ Cytron.ino
@@ -0,0 +1,13 @@
+#define pwm1 3
+#define dir1 4
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pwm1, OUTPUT);
+  pinMode(dir1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(dir1, HIGH);
+  analogWrite(pwm1, 150)
+}
